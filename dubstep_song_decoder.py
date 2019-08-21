974567cf5364d3840357c170f10d005527c3b1a3
rutaks <rootsum.dev@gmail.com>
2019-08-21 15:16:30 +0200

    Minor Changes

--- dubstep_song_decoder.py
+++ dubstep_song_decoder.py
@@ -1,3 +1,10 @@
+# Dubstep song titles often have WUB in their names
+# this decodes the song title by removing any WUB in it
+
+# eg: A song with words "I AM X" can transform into a dubstep remix as
+# "WUBWUBIWUBAMWUBWUBX" and cannot transform into "WUBWUBIAMWUBX"
+
+
 def song_decoder(song):
     return " ".join(song.replace("WUB", " ").strip().split())
 
